5fc03a1d2c0abd724cc94ebfd31a84a4cb08d1dd
Eli Schilling <eli.schilling@oracle.com>
2024-01-29 14:47:47 -0800

    Update func.py

--- func.py
+++ func.py
@@ -11,13 +11,16 @@ import io
 from fdk import response
 
 def handler(ctx, data: io.BytesIO=None):
+    print("Entering Python Hello World handler", flush=True)
     name = "World"
     try:
         body = json.loads(data.getvalue())
         name = body.get("name")
     except (Exception, ValueError) as ex:
-        print(str(ex))
-        pass
+        print(str(ex), flush=True)
+
+    print("Vale of name = ", name, flush=True)
+    print("Exiting Python Hello World handler", flush=True)
     return response.Response(
         ctx, response_data=json.dumps(
             {"message": "Hello {0}".format(name)}),
